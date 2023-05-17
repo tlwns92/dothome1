d848c3746e1e25d5f1fa9d335e6c29d7df722202
tlwns92 <50649607+tlwns92@users.noreply.github.com>
2023-05-17 15:09:25 +0900

    Add files via upload

---
+++ common/header.php
@@ -0,0 +1,35 @@
+
+
+<script>
+  function selectCategory(link) {
+    var links = document.querySelectorAll('#menu li');
+    for (var i = 0; i < links.length; i++) {
+      links[i].classList.remove('current_page_item');
+    }
+    var current_link = document.querySelector(`#menu a[href="${window.location.pathname}"]`).parentNode;
+    current_link.classList.add('current_page_item');
+  }
+</script>
+
+<div id="wrapper">
+  <div id="header-wrapper">
+    <div id="header">
+      <div id="logo">
+        <a href="index.php"><img class="main_logo" src="./images/img_logo.png" alt=""></a>
+      </div>
+    </div><!-- end #header -->
+  </div><!-- end #header-wrapper -->
+  <div id="menu">
+    <ul>
+      <!-- <li class="<?php echo ($_GET['category'] === 'index.php' || $_GET['category'] === 'home' || empty($_GET['category'])) ? 'current_page_item' : ''; ?>"><a href="index.php" onclick="selectCategory('index.php')">Home</a></li> -->
+      <!-- <li class="<?php echo ($_GET['category'] === './page/home' || $_GET['category'] === 'index' ) ? 'current_page_item' : ''; ?>"><a href="index.php?category=page/home">Home</a></li> -->
+      <li class="<?php echo (isset($_GET['category']) && ($_GET['category'] === 'page/home.php' || $_GET['category'] === '') ? 'current_page_item' : (!isset($_GET['category']) ? 'current_page_item' : '')); ?>"><a href="index.php" onclick="selectCategory('index.php')">Home</a></li>
+      <li class="<?php echo (isset($_GET['category']) && ($_GET['category'] === 'page/cardi')) ? 'current_page_item' : ''; ?>"><a href="index.php?category=page/cardi">Cardiovascular Border Analysis Demo</a></li>
+      <li class="<?php echo (isset($_GET['category']) && ($_GET['category'] === 'page/about')) ? 'current_page_item' : ''; ?>"><a href="index.php?category=page/about">About US</a></li>
+      <li class="<?php echo (isset($_GET['category']) && ($_GET['category'] === 'page/study')) ? 'current_page_item' : ''; ?>"><a href="index.php?category=page/study">Study Protocols</a></li>
+      <li class="<?php echo (isset($_GET['category']) && ($_GET['category'] === 'page/public')) ? 'current_page_item' : ''; ?>"><a href="index.php?category=page/public">Publications</a></li>
+      <li class="<?php echo (isset($_GET['category']) && ($_GET['category'] === 'page/present')) ? 'current_page_item' : ''; ?>"><a href="index.php?category=page/present">Presentations</a></li>
+      
+    </ul>
+  </div><!-- end #menu -->
+  </div><!-- end #wrapper -->
---
+++ common/sidebar.php
@@ -0,0 +1,55 @@
+<div id="sidebar-bg">
+			<div id="sidebar">
+
+			<!-- <?php echo $user_id . " " . $email . " " . $login_time . " " . $session_timeout;?> -->
+
+
+				<div class="region region-sidebar-first">
+					<div id="block-block-5" class="block block-block">
+				  
+						  <h2 >Contact Us</h2>
+					  
+					<div class="content">
+					  <p><strong>NYU Cardiovascular Clinical Research Center </strong><br />227 East 30th Street, New York, NY 10016 <br />Telephone: 212-263-4225 <br />Email: <a href="mailto:ISCHEMIA@nyulangone.org">ISCHEMIA@nyulangone.org</a></p>
+					</div>
+					
+				  </div> <!-- /.block -->
+				  <div id="block-user-login" class="block block-user">
+				  
+						  <h2 >User login</h2>
+					  
+					<div class="content">
+						<button type="button" class="button_signup" onClick="location.href='index.php?category=page/upload'">Upload</button>
+					</div>
+
+
+				 </div>
+				
+
+
+
+
+				  </div> <!-- /.block -->
+				  <div id="block-views-news-block" class="block block-views">
+				  
+						  <h2 >News</h2>
+					  
+					<div class="content">
+					  <div class="view view-news view-id-news view-display-id-block view-dom-id-5d0b7a368fc4d0f029d0a687fcd900d8">
+						  
+					
+					
+						<div class="view-content">
+						  <div class="views-row views-row-1 views-row-odd views-row-first views-row-last">
+						
+					<div class="views-field views-field-field-date">        <div class="field-content"><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2022-06-15T00:00:00-04:00">Wed, 06/15/2022 - 00:00</span></div>  </div>  
+					<div class="views-field views-field-title">        <span class="field-content"><a href="/news/ischemia-social-media">ISCHEMIA in the Social Media</a></span>  </div>  </div>
+					  </div></div></div></div></div>
+					
+
+			</div><!-- end #sidebar -->
+		</div>
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<!-- end #page -->
+</div>
